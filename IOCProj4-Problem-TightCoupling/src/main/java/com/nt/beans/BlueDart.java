cdac46e5c04bf27228ea811e9ddbabc89b9e4e1b
adibkhan768 <adibkhan768@gmail.com>
2019-12-21 21:55:24 +0530

    BlueDart commit

--- IOCProj4-Problem-TightCoupling/src/main/java/com/nt/beans/BlueDart.java
+++ IOCProj4-Problem-TightCoupling/src/main/java/com/nt/beans/BlueDart.java
@@ -5,6 +5,8 @@ import java.util.Date;
 public class BlueDart {
 	public BlueDart() {
 		System.out.println("BlueDart.0-param constructor");
+		System.out.println("This is the default constructor");
+		System.out.println("This constructor provided by jdk");
 	}
 	public void deliver(int oid) {
 		System.out.println(oid+" order is delivered to customer on "+new Date()+" by BlueDart");
